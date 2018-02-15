ecec14a279315f46782b9cdaa5d650cc6aa4499a
Jakov Sosic <jsosic@gmail.com>
2018-02-15 15:19:27 +0100

    Disable GeoIP dependency

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,7 @@
-# Class: ossim
+#
+# = Class: ossim
 #
 # This module manages ossim agent
-#
-# Requires:
-#
-#
-
-# Sample Usage:
-#   include ossim
 #
 class ossim (
   $package_ensure   = $ossim::params::package_ensure,
@@ -19,38 +13,38 @@ class ossim (
   $include_suricata = $ossim::params::include_suricata,
   $include_ossec    = $ossim::params::include_ossec,
   $verbose          = $ossim::params::verbose,
-  #$suricata_interfaces = $ossim::params::suricata_interfaces,
-
 ) inherits ossim::params {
-  include geoip
+
   if $include_snort == true {
-    include snort
-    $snort_interfaces = $snort::interfaces
+    include ::snort
+    $snort_interfaces = $::snort::interfaces
     ::ossim::snortunified{$snort_interfaces: }
   }
-  if $include_suricata == true {
 
+  if $include_suricata == true {
     include ::suricata
-    $suricata_interfaces = $suricata::interfaces
+    $suricata_interfaces = $::suricata::interfaces
     suricataunified{$suricata_interfaces: }
   }
+
   if $include_ossec == true {
-    include ossim::ossec
+    include ::ossim::ossec
   }
+
   package { 'ossim-agent':
     ensure  => $package_ensure,
-    #require => Package['GeoIP'],
   }
+
   file { '/etc/ossim/agent/config.cfg':
     ensure  => file,
     content => template('ossim/config.cfg.erb'),
     require => Package['ossim-agent'],
   }
+
   service { 'ossim-agent':
     ensure    => running,
     enable    => true,
-    provider  => redhat,
-    require   => File['/etc/ossim/agent/config.cfg'],
     subscribe => File['/etc/ossim/agent/config.cfg'],
   }
+
 }
